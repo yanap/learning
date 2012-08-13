d9163460db928e327fc54f00706845b221e51eb9
yanap <yanap1214@gmail.com>
2012-08-13 22:28:32 +0900

    input, output

--- ruby/hello.rb
+++ ruby/hello.rb
@@ -18,3 +18,13 @@ puts      (           # 括弧が開いたままなので次の行に続く
 puts   \
      "Hello, " + # 2項演算子の右辺がないので次の行に行く
    "World!"
+
+#input, output
+name = gets 					#標準入力から１行入力
+print "Hello, #{name}\n" 		#文字列中の式展開
+printf "Hello, %s\n", name 		#C言語風の文字列フォーマット出力
+message = "Hello, %s" % name 	#文字列フォーマットの別の書き方
+puts message					#putsメソッドは最後に改行を出力する
+$stderr.puts message 			#標準エラー出力へ
+$stdout.puts message 			#標準出力を明示しても良い
+$stdout.class #=> IO			これらはIOクラスのインスタンス
