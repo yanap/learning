f9822c06baee65f9a28110ae0fc6089bc0994170
yanap <yanap1214@gmail.com>
2012-09-08 16:57:32 +0900

    add example

---
+++ ruby/example/1-1.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+str1 = "string"
+str2 = "string"
+
+p str1, str2
+
+p str1.object_id
+p str2.object_id
+p str1 == str2 
+p str1.equal?(str2)
+
+p  'testing str1, str2';
