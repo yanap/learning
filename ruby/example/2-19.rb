d12f6e69bcd99e7b95d119103422e74461bf2a67
yanap <yanap1214@gmail.com>
2012-10-20 00:19:30 +0900

    add example

---
+++ ruby/example/2-19.rb
@@ -0,0 +1,2 @@
+params = { rin: 5, kimiko: 7, kayo: nil }
+p params #=> {:rin => 5, :kimiko => 7, :kayo => nil}
---
+++ ruby/example/2-20.rb
@@ -0,0 +1,12 @@
+book_to_author = {
+	"Ruby in Nutshell" => "Flanagan",
+	"Programming Ruby" => "Thomas",
+	"AWDwR" => "Thomas",
+}
+p book_to_author["Programming Ruby"] #=> "Thomas"
+# 存在しないキーの場合
+p book_to_author["Programming Perl"] #=> nil
+# 既存のキーの更新
+book_to_author["Ruby in Nutshell"] = ["Flanagan", "Matz"]
+# 新しいキーの登録
+book_to_author["The Ruby Way"] = "Fulton"
