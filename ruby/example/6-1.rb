76573b9d3474b0dd1a9d09dddb5040c319c889fd
yanap <yanap1214@gmail.com>
2012-12-05 00:02:41 +0900

    add 6

---
+++ ruby/example/6-1.rb
@@ -0,0 +1,9 @@
+cattle = "yahoo" # Stringオブジェクトへの参照を代入
+cattle.length #=> 5 変数を通じてメソッドを呼び出し
+animal = cattle #=> "yahoo"
+cattle[2] = ?p
+p cattle #=> "yapoo"
+p animal #=> "yapoo"
+cattle.equal? animal #=> true オブジェクトとして同一
+
+
---
+++ ruby/example/6-2.rb
@@ -0,0 +1,10 @@
+def describe(name)
+  name = name.dup
+  puts "This is a #{name}." #=> This is a yahoo.
+  name[2] = ?p
+  puts "This is a #{name}." #=> This is a yapoo.
+end
+CATTLE = "yahoo"
+decribe(CATTLE)
+p CATTLE #=> "yahoo"
+
