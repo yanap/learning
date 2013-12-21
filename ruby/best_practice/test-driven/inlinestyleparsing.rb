e8884395172a469127518f5ff0ac15fdc147ba8c
yanap <yanap1214@gmail.com>
2013-12-22 02:00:13 +0900

    done

--- ruby/best_practice/test-driven/inlinestyleparsing.rb
+++ ruby/best_practice/test-driven/inlinestyleparsing.rb
@@ -4,35 +4,51 @@ require 'test/unit'
 require_relative 'test_unit_extensions'
 
 class Prawn::Document
-  def parse_inline_styles(text)
-    text.split( %r{(</?[ib]>)} ).delete_if{|x| x.empty?}
+  module Text::StyleParser
+    extend self
+
+    TAG_PATTERN = %r{(</?[ib]>)}
+
+    def process(text)
+      text.split(TAG_PATTERN).delete_if{|x| x.empty? }
+    end
+
+    def style_tag?(text)
+      !!(text =~ TAG_PATTERN)
+    end
   end
 end
 
 # test
 class TestInlineStyleParsing < Test::Unit::TestCase
+
+  def setup
+    @parser = Prawn::Document::Text::StyleParser
+  end
+
   must "parse italic tags" do
-    @pdf = Prawn::Document.new
     assert_equal ["Hello ", "<i>", "Fine", "</i>", " World"],
-      @pdf.parse_inline_styles("Hello <i>Fine</i> World")
+      @parser.process("Hello <i>Fine</i> World")
   end
 
   must "parse bold tags" do
-    @pdf = Prawn::Document.new
     assert_equal ["Some very ", "<b>", "bold text", "</b>"],
-      @pdf.parse_inline_styles("Some very <b>bold text</b>")
+      @parser.process("Some very <b>bold text</b>")
+  end
+
+  must "parse mixed italic and bold tags" do
+    assert_equal ["Hello ", "<i>", "Fine ", "<b>", "World", "</b>", "</i>"],
+      @parser.process("Hello <i>Fine <b>World</b></i>")
   end
 
   must "not split out other tags than <i>, <b>, </i>, </b>" do
-    @pdf = Prawn::Document.new
     assert_equal ["Hello <indigo>Ch", "</b>", "arl", "</b>", "ie</indigo>"],
-      @pdf.parse_inline_styles("Hello <indigo>Ch</b>arl</b>ie</indigo>")
+      @parser.process("Hello <indigo>Ch</b>arl</b>ie</indigo>")
   end
 
   must "be able to check whether a string needs to be parsed" do
-    @pdf = Prawn::Document.new
-    assert !@pdf.send(:style_tag?, "Hello World")
-    assert @pdf.send(:style_tag?, "Hello <i>Fine</i> World")
+    assert @parser.style_tag?("Hello <i>Fine</i> World")
+    assert !@parser.style_tag?("Hello World")
   end
 
 end
