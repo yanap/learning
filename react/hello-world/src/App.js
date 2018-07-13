ff14ececea1bfbf41be6cbcd2871802ae02fbd84
yanap <yanap1214@gmail.com>
2018-07-13 16:28:29 +0900

    add react hello world
    https://reactjs.org/blog/2016/07/22/create-apps-with-no-configuration.html
    
    create-react-app hello-world

---
+++ react/hello-world/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+	<section>
+	<details>
+	<summary>開閉</summary>
+	ああああ
+	いいいい
+	うううう
+	ええええ
+	おおおお
+	</details>
+	</section>
+      </div>
+    );
+  }
+}
+
+export default App;
