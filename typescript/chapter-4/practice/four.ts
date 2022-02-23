2864363e0ad1000fbc800ef92f0c6479c4b63bb5
yanap <yanap1214@gmail.com>
2022-02-24 08:57:02 +0900

    p4

--- typescript/chapter-4/practice/four.ts
+++ typescript/chapter-4/practice/four.ts
@@ -1,3 +1,21 @@
 /*
-4. [難問] 
+4. [難問] callの実装(「4.2.5.2 制限付きポリモーフィズムを使って、可変長引数をモデル化する」
+    を参照)を、２番目の引数がstringである関数についてだけ機能すルように書き換えてください。
+    そうではない関数を渡すと、コンパイル時にエラーとなるようにします。
 */
+
+function call<T extends [unknown, string, ...unknown[]], R>(
+    f: (...args: T) => R,
+    ...args: T
+): R {
+    return f(...args)
+}
+
+// 難問だ...
+
+function fill(length: number, value: string): string[] {
+    return Array.from({length}, () => value)
+}
+
+call(fill, 10, 'a') // string[]
+
