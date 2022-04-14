19d66ff380d729c54818195bf4b3ae9e4b1792ed
yanap <yanap1214@gmail.com>
2022-04-14 17:15:41 +0900

    4

---
+++ python/green.py
@@ -0,0 +1,12 @@
+small = False
+green = True
+if small:
+    if green:
+        print("pea")
+    else:
+        print("cherry")
+else:
+    if green:
+        print("watermelon")
+    else:
+        print("pumpkin")
---
+++ python/guess.py
@@ -0,0 +1,9 @@
+secret = 7
+guess = 5
+if guess < secret:
+    print('too low')
+elif guess > secret:
+    print('too high')
+else:
+    print('just right')
+
