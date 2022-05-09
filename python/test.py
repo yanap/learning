bbc16531c8db051c22240e7317d4b8e28e739e95
yanap <yanap1214@gmail.com>
2022-05-09 19:53:43 +0900

    509

--- python/test.py
+++ python/test.py
@@ -1,1 +1,16 @@
 print(27)
+
+def test(func):
+	def new_func(*args, **kwargs):
+		print('start')
+		result = func(*args, **kwargs)
+
+		print('end')
+		return result
+	return new_func
+
+@test
+def greeting():
+	print("Greetings, Earthling")
+
+greeting()
