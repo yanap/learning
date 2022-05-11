8f01338d8d014ef500671e58b84159e9bbc84c52
yanap <yanap1214@gmail.com>
2022-05-11 22:17:12 +0900

    511

---
+++ python/BearRabbitOctothorpe.py
@@ -0,0 +1,18 @@
+class Bear:
+	def eats(self):
+		return 'barries'
+
+class Rabbit:
+	def eats(self):
+		return 'clover'
+
+class Octothorpe:
+	def eats(self):
+		return 'campers'
+
+b = Bear()
+r = Rabbit()
+o = Octothorpe()
+print(b.eats())
+print(r.eats())
+print(o.eats())
