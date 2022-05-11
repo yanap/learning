737a6fa66224e2343c41eb871f0e7058f3cba13a
yanap <yanap1214@gmail.com>
2022-05-11 22:12:14 +0900

    anotation

--- python/elementclass.py
+++ python/elementclass.py
@@ -1,13 +1,24 @@
 class Element:
 	def __init__(self, name, symbol, number):
-		self.name = name
-		self.symbol = symbol
-		self.number = number
+		self.__name = name
+		self.__symbol = symbol
+		self.__number = number
+	@property
+	def name(self):
+		return self.__name
+	@property
+	def symbol(self):
+		return self.__symbol
+	@property
+	def number(self):
+		return self.__number
+
 
 hydrogen = Element('Hydrogen', 'H', 1)
 
-el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
-
-hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
+# el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
+# hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
 
 print(hydrogen.name)
+print(hydrogen.symbol)
+print(hydrogen.number)
