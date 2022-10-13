021ff8dac0c09c0b8a327d72884522bd0a9c7a93
yanap <yanap1214@gmail.com>
2022-10-13 16:34:16 +0900

    create_before_destroy

--- terraform/basic/main.tf
+++ terraform/basic/main.tf
@@ -1,15 +1,19 @@
 resource "google_compute_instance" "default" {
-  name         = "test"
+  name         = "test2"
   machine_type = "f1-micro"
   zone         = "asia-northeast1-a"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-11"
     }
   }
 
   network_interface {
     network = "default"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
